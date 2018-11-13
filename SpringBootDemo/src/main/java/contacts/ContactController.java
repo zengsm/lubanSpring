142d0ecb6d33fc28221b02ef4e15990bf879723f
zsm <13751740715@163.com>
2018-11-13 21:28:27 +0800

    springBoot-version1

---
+++ SpringBootDemo/src/main/java/contacts/ContactController.java
@@ -0,0 +1,35 @@
+package contacts;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class ContactController {
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	
+//	public ContactController(ContactRepository contactRepository){
+//		this.contactRepository = contactRepository;
+//	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String home(Map<String,Object> map){
+		List<Contact> contacts = contactRepository.findAll();
+		map.put("contacts", contacts);
+		return "home";
+	}
+	
+	@RequestMapping(value="aaa",method=RequestMethod.POST)
+	public String submit(Contact contact){
+		contactRepository.save(contact);
+		return "redirect:/";
+	}
+}
